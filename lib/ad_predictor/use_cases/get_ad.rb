1e68b9817dab051d91b7e5c6414bf9907068cbfb
Brian Provost <brianmprovost@gmail.com>
2014-06-17 11:18:05 -0500

    GetAd returns the ad with the highest clickthrough for given parameters.

--- lib/ad_predictor/use_cases/get_ad.rb
+++ lib/ad_predictor/use_cases/get_ad.rb
@@ -1,7 +1,27 @@
 module AdPredictor
   class GetAd < UseCase
     def run(inputs)
+      return failure(:no_input) if inputs == {}
 
+      db = AdPredictor.db
+      # All impressions that match params
+      impression_matches = db.find_impressions(inputs)
+
+      # Get all unique ad names in impressions
+      ads = impression_matches.uniq { |impression| impression.ad }
+
+      # Map ads array to ad name and clickthrough percentage
+      clickthroughs = []
+
+      ads.each do |impression|
+        ad_matches = impression_matches.select { |impression_match| impression_match.ad == impression.ad }
+        clickthroughs.push(ad: impression.ad, clickthrough: AdPredictor::GetClickthrough.run(ad_matches).clickthrough)
+      end
+
+      clickthroughs.sort_by! { |pair| pair[:clickthrough] }
+
+      # Return name of ad with highest clickthrough
+      success ad: clickthroughs.last[:ad]
     end
   end
 end
--- spec/get_ad_spec.rb
+++ spec/get_ad_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe AdPredictor::GetAd do
   let(:db) { AdPredictor.db }
+  before do
+    db.clear
+    AdPredictor::ImportCSV.run(path: './data/ad_picker_test.csv')
+  end
+
+  it "returns the ad with the highest clickthrough" do
+    result = subject.run(platform: "iOS")
+    expect(result.success?).to eq true
+    expect(result.ad).to eq "Candy Smash"
+  end
 end
